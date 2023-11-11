0a3bb1f3133c9db0a3c407e680293c5c73663085
MarcosDCP <zs20018147@estudiantes.uv.mx>
2023-11-11 13:12:03 -0600

    Diseño de interfaz Login final

--- src/clienteescritoriocupones/ClienteEscritorioCupones.java
+++ src/clienteescritoriocupones/ClienteEscritorioCupones.java
@@ -1,10 +1,13 @@
 package clienteescritoriocupones;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -13,12 +16,31 @@ import javafx.stage.Stage;
  * @author marcos
  */
 public class ClienteEscritorioCupones extends Application {
-    
+    private double xOffset = 0;
+    private double yOffset = 0;
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
         
         Scene scene = new Scene(root);
+       
+        stage.initStyle(StageStyle.DECORATED.UNDECORATED);
+        root.setOnMousePressed(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+            
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>(){
+               @Override
+               public void handle(MouseEvent event){
+                   stage.setX(event.getScreenX() -xOffset);
+                   stage.setY(event.getScreenY() -yOffset);
+               }
+            
+        });
         
         stage.setScene(scene);
         stage.show();
--- src/clienteescritoriocupones/FXMLLoginController.java
+++ src/clienteescritoriocupones/FXMLLoginController.java
@@ -1,33 +1,94 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clienteescritoriocupones;
+//------------ LIBRERIAS USADAS --------------//
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.awt.Desktop;
+import java.net.URI;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Border;
+import javafx.stage.Stage;
 
-/**
- *
- * @author erric
- */
 public class FXMLLoginController implements Initializable {
-    
+
+    //----------------- DECALARCION DE COMPONENTES FXML -------------------//
     private Label label;
-    
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    @FXML
+    private JFXButton btn_api;
+    @FXML
+    private JFXPasswordField tf_contraseña;
+    @FXML
+    private JFXTextField tf_nombreUsuario;
+    @FXML
+    private JFXButton btn_ingresar;
+    @FXML
+    private JFXButton btn_frontend;
+    @FXML
+    private JFXButton btn_cerrarVentana;
+
+//--------- Método initialize ---------//
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        colocarImagenBotones();
+        
+    }
+
+//--------- ActionEvents ---------//
+    @FXML
+    private void btnCerrarVentana(ActionEvent event) {
+        cerrarVentana();
+    }
+    @FXML
+    private void btnAbrirApi(ActionEvent event) {
+         String url = "https://github.com/MarcosYahirDCP/APIcuponsmart";
+         abrirLink(url);
+    }
+
+    @FXML
+    private void btnAbrirFrontEnd(ActionEvent event) {
+         String url = "https://github.com/TheMandiloreanEAHA/ClienteEscritorioCupones";
+         abrirLink(url);
+    }
+//--------- Metodo para asignar iconos a los botones ---------//
+    private void colocarImagenBotones() {
+        // creacionn de los links de la imagen a partir de un recurso
+        URL linkAPI = getClass().getResource("/clienteescritoriocupones/recursos/icon_API.png");
+        URL linkCerrar = getClass().getResource("/clienteescritoriocupones/recursos/icon_Cerrar.png");
+        URL linkFrontend = getClass().getResource("/clienteescritoriocupones/recursos/icon_Frontend.png");
+
+        // creacion de la imagen a partir de los links
+        Image imagenAPI = new Image(linkAPI.toString(), 40, 40, false, false);
+        Image imagenCerrar = new Image(linkCerrar.toString(), 20, 20, false, false);
+        Image imagenFrontend = new Image(linkFrontend.toString(), 40, 40, false, false);
+
+        // asignación de la imagen a los botones
+        btn_api.setGraphic(new ImageView(imagenAPI));
+        btn_frontend.setGraphic(new ImageView(imagenFrontend));
+        btn_cerrarVentana.setGraphic(new ImageView(imagenCerrar));
+        
+    }
+//--------- Método para cerrar la ventana ---------//
+    private void cerrarVentana() {
+        Stage escenario = (Stage) btn_api.getScene().getWindow();
+        escenario.close();
+    }
+//--------- Metodos para abrir link desde el boton ---------//
+    private void abrirLink(String url) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
